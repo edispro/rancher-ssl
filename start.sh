20181c121b53ff088faac27b16bcd7cf7d88f6b5
pong-nb <sompong@edispro.com>
2019-02-23 18:20:59 +0700

    fix

--- start.sh
+++ start.sh
@@ -1,6 +1,3 @@
-if [ ! -d /etc/letsencrypt/live/$DOMAIN ]; then
-  letsencrypt certonly --noninteractive --agree-tos --email $EMAIL --standalone -d $DOMAIN
-fi
 
 sed "s/<DOMAIN>/$DOMAIN/" ./nginx.conf > /etc/nginx/nginx.conf
 nginx -g "daemon off; error_log /dev/stderr notice;"
