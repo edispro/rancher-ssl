217c648c1c49aba65b4f26d1e65307f73f35806e
pong-nb <sompong@edispro.com>
2019-02-23 20:11:14 +0700

    fix

--- start.sh
+++ start.sh
@@ -2,9 +2,9 @@
 file="/etc/nginx/saveconf"
 if [ ! -f "$file" ]
 then
-sed "s/<DOMAIN>/$DOMAIN/" ./nginx.conf > /tmp/nginx.conf
-sed "s/<PORTSSL>/$PORTSSL/" /tmp/nginx.conf > /tmp/nginx.conf_1
-sed "s/<PORT>/$PORT/" /tmp/nginx.conf_1 > /etc/nginx/nginx.conf
+sed "s/<DOMAIN>/$DOMAIN/" ./nginx.conf > ./nginx.conf_1
+sed "s/<PORTSSL>/$PORTSSL/" ./nginx.conf_1 > ./nginx.conf_2
+sed "s/<PORT>/$PORT/" ./nginx.conf_2 > /etc/nginx/nginx.conf
 touch $file
 fi
 nginx -g "daemon off; error_log /dev/stderr notice;"
