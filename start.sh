5d435c512d7e5204a3fc2a4e5399f939efafd88e
pong-nb <sompong@edispro.com>
2019-02-23 19:45:40 +0700

    fix

--- start.sh
+++ start.sh
@@ -3,5 +3,6 @@ file="/etc/nginx/saveconf"
 if [ ! -f "$file" ]
 then
 sed "s/<DOMAIN>/$DOMAIN/" ./nginx.conf > /etc/nginx/nginx.conf
+touch $file
 fi
 nginx -g "daemon off; error_log /dev/stderr notice;"
